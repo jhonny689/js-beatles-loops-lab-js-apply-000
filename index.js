dcda315122eec9ce3edd880b04d90e6fff607b36
blessed-publisher-8631 <jhonny.chamoun@gmail.com>
2020-05-16 20:26:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments)
 {
-  var emptyArr;
+  var resArr;
   for(var i = 0; i < musicians.length; i++)
   {
     emptyArr[i] = musicians[i] + " plays " + instruments[i];
   }
+  return resArr;
 }
