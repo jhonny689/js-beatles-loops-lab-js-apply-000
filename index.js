32211354b6dae1a5ccea61f813c9057ddb5af072
blessed-publisher-8631 <jhonny.chamoun@gmail.com>
2020-05-16 20:44:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(musicians, instruments)
   var resArr;
   for(var i = 0; i < musicians.length; i++)
   {
-    emptyArr[i] = musicians[i] + " plays " + instruments[i];
+    resArr[i] = musicians[i] + " plays " + instruments[i];
   }
   return resArr;
 }
